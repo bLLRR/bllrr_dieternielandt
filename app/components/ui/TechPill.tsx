c0f01d7174af0095b05896b4269d2ca8558e1e14
Dieter Nielandt <dieter.nielandt@gmail.com>
2025-12-12 22:54:29 +0100

    chore: create TechPill component for displaying technology with icon

---
+++ app/components/ui/TechPill.tsx
@@ -0,0 +1,23 @@
+import { Technology } from '@/types/Technology';
+
+import TechIcon from './TechIcon';
+import Pill from './Pill';
+
+interface TechPillProps {
+  technology: Technology;
+  width?: number;
+  height?: number;
+}
+
+export default function TechPill({
+  technology,
+  height = 15,
+  width = 15,
+}: TechPillProps) {
+  return (
+    <Pill>
+      <TechIcon icon={technology.icon} height={height} width={width} />
+      {technology.name}
+    </Pill>
+  );
+}
