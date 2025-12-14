3c454498e715d9abfc4a09395d5034318bac4ebb
Dieter Nielandt <dieter.nielandt@gmail.com>
2025-12-14 18:22:07 +0100

    Improve LinkButton dark mode styling
    Added dark mode classes to LinkButton and its icon container for better appearance in dark themes. Adjusted border and background colors to enhance visual consistency.

--- app/components/ui/Link.tsx
+++ app/components/ui/Link.tsx
@@ -18,17 +18,17 @@ export default function LinkButton({
     <Link
       href={href}
       className={clsx(
-        `rounded-2xl border border-gray-300 px-2.5 py-2`,
+        `items-center rounded-2xl border border-gray-300 px-2.5 py-2 dark:border-gray-700 dark:bg-gray-800`,
         className,
       )}
     >
       {icon && (
         <span
           className={clsx(
-            `mr-1 rounded-sm border border-gray-300 bg-gray-100 px-1`,
+            `mr-1 items-center rounded-sm border border-gray-300 bg-gray-100 px-1 dark:border-none dark:bg-gray-800`,
           )}
         >
-          <i className={`${icon} text-gray-400`} />
+          <i className={`${icon} text-gray-400 dark:text-gray-500`} />
         </span>
       )}
       {children}
