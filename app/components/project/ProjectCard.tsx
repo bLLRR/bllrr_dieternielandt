6fa3e3273dd0a00c0433702d3bc15894f547d00c
Dieter Nielandt <dieter.nielandt@gmail.com>
2025-12-14 18:12:43 +0100

    Refactor ProjectCard icon color handling
    Introduced a colorClass variable to manage icon color classes using clsx, improving readability and maintainability. Also updated styling for dark mode compatibility and adjusted className usage for the project icon.

--- app/components/project/ProjectCard.tsx
+++ app/components/project/ProjectCard.tsx
@@ -16,6 +16,9 @@ export default function ProjectCard({
   project,
   className = '',
 }: ProjectCardProps) {
+  const colorClass = clsx(
+    `text-${project.projectIcon?.color}-500 dark:text-${project.projectIcon?.color}-700`,
+  );
   return (
     <Link
       href={`/projects/${project.id}`}
@@ -24,14 +27,13 @@ export default function ProjectCard({
       <Card className="flex h-full flex-col">
         <div className="mb-4 items-center">
           <div className="flex justify-between">
-            <div className="flex flex-wrap">
-              <span className="rounded-sm border border-gray-300 bg-gray-100 px-1">
+            <div className="flex flex-wrap items-center">
+              <span className="mt-0.5 rounded-sm border border-gray-300 bg-gray-100 px-1 dark:border-gray-600 dark:bg-gray-700">
                 {project.projectIcon && (
                   <i
                     className={clsx(
                       project?.projectIcon.name,
-                      project?.projectIcon?.color &&
-                        `text-${project.projectIcon.color}-500`,
+                      project?.projectIcon?.color && className,
                     )}
                   />
                 )}
