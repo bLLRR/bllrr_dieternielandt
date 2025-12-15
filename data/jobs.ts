14167119afeef12c37a05efb6aa8d82012e58d2a
Dieter Nielandt <dieter.nielandt@gmail.com>
2025-12-15 13:13:43 +0100

    Add jobs data with job entries
    Introduces a new jobs.ts file exporting an array of job objects, each containing id, title, company, time, and associated projectIds. This provides structured job data for use in the application.

---
+++ data/jobs.ts
@@ -0,0 +1,18 @@
+import { Job } from '@/types/Job';
+
+export const jobs: Job[] = [
+  {
+    id: 1,
+    title: 'Full stack developer',
+    company: 'FG-Software',
+    time: 'Januari 2021 - Juli 2025',
+    projectIds: [2, 3, 4, 5],
+  },
+  {
+    id: 2,
+    title: 'Developer',
+    company: 'C-Supply',
+    time: 'Mei 2019 - December 2020',
+    projectIds: [6, 7, 8, 9],
+  },
+];
