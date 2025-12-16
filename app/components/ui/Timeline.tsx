020191072fabf96ac581e59034c46856c909c876
Dieter Nielandt <dieter.nielandt@gmail.com>
2025-12-16 15:09:09 +0100

    feat: update ItemProps to allow React nodes for title and text, and enhance Timeline component structure

--- app/components/ui/Timeline.tsx
+++ app/components/ui/Timeline.tsx
@@ -1,6 +1,6 @@
 interface ItemProps {
   time: string;
-  title: string;
+  title: string | React.ReactNode;
   subtitle?: string;
   text: string | React.ReactNode;
 }
@@ -8,7 +8,7 @@ interface ItemProps {
 function Item({ time, title, subtitle, text }: ItemProps) {
   return (
     <li className="ms-4 mb-10">
-      <div className="border-buffer absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border bg-slate-300"></div>
+      <div className="border-buffer absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border bg-slate-300 dark:border-slate-700 dark:bg-slate-800"></div>
       <time className="text-body text-sm leading-none font-normal">{time}</time>
       <h2 className="text-heading my-2 text-xl font-semibold">{title}</h2>
       {subtitle && (
@@ -20,7 +20,11 @@ function Item({ time, title, subtitle, text }: ItemProps) {
 }
 
 export default function Timeline({ children }: { children?: React.ReactNode }) {
-  return <ol className="border-default relative border-s">{children}</ol>;
+  return (
+    <ol className="border-default relative border-s border-slate-300 dark:border-slate-700">
+      {children}
+    </ol>
+  );
 }
 
 Timeline.Item = Item;
