4967b849ae148e6dde30c9052b79f1662c89c8f8
Dieter Nielandt <dieter.nielandt@gmail.com>
2025-12-17 06:58:07 +0100

    feat: simplify Card component structure and styling

--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -7,14 +7,5 @@ export default function Card({
   children: React.ReactNode;
   className?: string;
 }) {
-  return (
-    <div
-      className={clsx(
-        'rounded-lg border border-gray-300 p-5 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300',
-        className,
-      )}
-    >
-      {children}
-    </div>
-  );
+  return <div className={clsx('card', className)}>{children}</div>;
 }
